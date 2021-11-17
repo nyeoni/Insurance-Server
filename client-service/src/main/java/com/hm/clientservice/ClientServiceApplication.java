73af3cedcc463290ceceddea1904c91149318b16
Yunsik-Choi <choiys0311@gmail.com>
2021-11-18 01:12:10 +0900

    [feat]: Register Eureka
    Eureka 등록 및 각 서비스 설정 정보 변경

--- client-service/src/main/java/com/hm/clientservice/ClientServiceApplication.java
+++ client-service/src/main/java/com/hm/clientservice/ClientServiceApplication.java
@@ -2,8 +2,10 @@ package com.hm.clientservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class ClientServiceApplication {
 
 	public static void main(String[] args) {
